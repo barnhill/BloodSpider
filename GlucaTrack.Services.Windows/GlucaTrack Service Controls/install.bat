ce475d9f5e9e08fdfcc0ee6c1061a231310bb84c
bradbarnhill@hotmail.com <bradbarnhill@hotmail.com>
2013-05-22 07:39:55 -0500

    changed install bat to read from %usersprofile% and not a hardcoded desktop directory.

--- GlucaTrack.Services.Windows/GlucaTrack Service Controls/install.bat
+++ GlucaTrack.Services.Windows/GlucaTrack Service Controls/install.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil C:\Users\bbarnhill\Desktop\Dropbox\Projects\GlucaTrack\GlucaTrack.Services.Windows\bin\Debug\GlucaTrack.Services.Windows.exe
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil %userprofile%\desktop\Dropbox\Projects\GlucaTrack\GlucaTrack.Services.Windows\bin\Debug\GlucaTrack.Services.Windows.exe
 net start "GlucaTrack Detector"
--- GlucaTrack.Services.Windows/GlucaTrack Service Controls/remove.bat
+++ GlucaTrack.Services.Windows/GlucaTrack Service Controls/remove.bat
@@ -1,1 +1,1 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil /u C:\Users\bbarnhill\Desktop\Dropbox\Projects\GlucaTrack\GlucaTrack.Services.Windows\bin\Debug\GlucaTrack.Services.Windows.exe
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil /u %userprofile%\Desktop\Dropbox\Projects\GlucaTrack\GlucaTrack.Services.Windows\bin\Debug\GlucaTrack.Services.Windows.exe
