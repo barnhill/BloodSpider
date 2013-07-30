1cae5363959da60548ea114f082db0f3cd48ba91
bbarnhill <bradbarnhill@hotmail.com>
2013-07-29 20:39:20 -0500

    moving the notify icon to a new namespace

--- GlucaTrack.Services.Windows.NotifyIcon/Properties/AssemblyInfo.cs
+++ GlucaTrack.Services.Windows.NotifyIcon/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GlucaTrack_ServiceNotifyIcon")]
+[assembly: AssemblyTitle("GlucaTrack.Services.Windows.NotifyIcon")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GlucaTrack_ServiceNotifyIcon")]
+[assembly: AssemblyCompany("GlucaTrack")]
+[assembly: AssemblyProduct("GlucaTrack.Services.Windows.NotifyIcon")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
